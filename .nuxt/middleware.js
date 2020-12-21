7150ad7ad7bfb47e01a720a0a209103e01f40082
Maciej Klimowicz <m.klimowicz@goldensubmarine.com>
2020-12-21 13:55:36 +0100

    linguist fix

--- .nuxt/middleware.js
+++
@@ -1,6 +0,0 @@
-const middleware = {}
-
-middleware['redirects'] = require('../middleware/redirects.js')
-middleware['redirects'] = middleware['redirects'].default || middleware['redirects']
-
-export default middleware
