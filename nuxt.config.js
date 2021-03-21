eb60fb5774d35d99b04d689f1ec3f7c82a98a55c
Maciej Klimowicz <m.klimowicz@goldensubmarine.com>
2021-03-21 21:59:21 +0100

    netlify

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,8 +1,8 @@
 const apiUrl = process.env.API_URL || "http://localhost:1337";
 
 export default {
-  target: 'server',
-  ssr: 'true',
+  target: 'static',
+  // ssr: 'true',
   head: {
     title: process.env.APP_TITLE,
     meta: [{
